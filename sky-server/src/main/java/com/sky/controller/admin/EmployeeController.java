e96fae3300c3d0c61ec6e073bf253634fa6d2bb0
Passeng <1151753695@qq.com>
2023-10-09 22:42:49 +0800

    新增员工

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -8,6 +9,8 @@ import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,54 +24,71 @@ import java.util.Map;
 /**
  * 员工管理
  */
+@Api(tags = "员工相关接口")
 @RestController
 @RequestMapping("/admin/employee")
 @Slf4j
 public class EmployeeController {
 
-    @Autowired
-    private EmployeeService employeeService;
-    @Autowired
-    private JwtProperties jwtProperties;
+	@Autowired
+	private EmployeeService employeeService;
+	@Autowired
+	private JwtProperties jwtProperties;
 
-    /**
-     * 登录
-     *
-     * @param employeeLoginDTO
-     * @return
-     */
-    @PostMapping("/login")
-    public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
-        log.info("员工登录：{}", employeeLoginDTO);
+	/**
+	 * 登录
+	 *
+	 * @param employeeLoginDTO
+	 * @return
+	 */
+	@ApiOperation("员工登录")
+	@PostMapping("/login")
+	public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
+		log.info("员工登录：{}", employeeLoginDTO);
 
-        Employee employee = employeeService.login(employeeLoginDTO);
+		Employee employee = employeeService.login(employeeLoginDTO);
 
-        //登录成功后，生成jwt令牌
-        Map<String, Object> claims = new HashMap<>();
-        claims.put(JwtClaimsConstant.EMP_ID, employee.getId());
-        String token = JwtUtil.createJWT(
-                jwtProperties.getAdminSecretKey(),
-                jwtProperties.getAdminTtl(),
-                claims);
+		//登录成功后，生成jwt令牌
+		Map<String, Object> claims = new HashMap<>();
+		claims.put(JwtClaimsConstant.EMP_ID, employee.getId());
+		String token = JwtUtil.createJWT(
+				jwtProperties.getAdminSecretKey(),
+				jwtProperties.getAdminTtl(),
+				claims);
 
-        EmployeeLoginVO employeeLoginVO = EmployeeLoginVO.builder()
-                .id(employee.getId())
-                .userName(employee.getUsername())
-                .name(employee.getName())
-                .token(token)
-                .build();
+		EmployeeLoginVO employeeLoginVO = EmployeeLoginVO.builder()
+				.id(employee.getId())
+				.userName(employee.getUsername())
+				.name(employee.getName())
+				.token(token)
+				.build();
 
-        return Result.success(employeeLoginVO);
-    }
+		return Result.success(employeeLoginVO);
+	}
 
-    /**
-     * 退出
-     *
-     * @return
-     */
-    @PostMapping("/logout")
-    public Result<String> logout() {
-        return Result.success();
-    }
+	/**
+	 * 退出
+	 *
+	 * @return
+	 */
+	@ApiOperation("员工退出")
+	@PostMapping("/logout")
+	public Result<String> logout() {
+		return Result.success();
+	}
+
+	/**
+	 * 新增员工
+	 *
+	 * @param employeeDTO
+	 * @return
+	 */
+	@ApiOperation("新增员工")
+	@PostMapping("")
+	public Result saveEmployee(@RequestBody EmployeeDTO employeeDTO) {
+		log.info("新增员工：{}", employeeDTO);
+		employeeService.saveEmployee(employeeDTO);
+		return Result.success();
+	}
 
 }
