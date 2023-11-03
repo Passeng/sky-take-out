c8934894e16fc9aa420d64493739ebc9e87933a8
Passeng <1151753695@qq.com>
2023-11-03 17:26:51 +0800

    店铺状态设置

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -27,65 +27,83 @@ import java.util.List;
 @Slf4j
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
-	@Autowired
-	private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
+    @Autowired
+    private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
 
-	/**
-	 * 注册自定义拦截器
-	 *
-	 * @param registry
-	 */
-	protected void addInterceptors(InterceptorRegistry registry) {
-		log.info("开始注册自定义拦截器...");
-		registry.addInterceptor(jwtTokenAdminInterceptor)
-				.addPathPatterns("/admin/**")
-				.excludePathPatterns("/admin/employee/login");
-	}
+    /**
+     * 注册自定义拦截器
+     *
+     * @param registry
+     */
+    protected void addInterceptors(InterceptorRegistry registry) {
+        log.info("开始注册自定义拦截器...");
+        registry.addInterceptor(jwtTokenAdminInterceptor)
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin/employee/login");
+    }
 
-	/**
-	 * 通过knife4j生成接口文档
-	 *
-	 * @return
-	 */
-	@Bean
-	public Docket docket() {
-		ApiInfo apiInfo = new ApiInfoBuilder()
-				.title("苍穹外卖项目接口文档")
-				.version("2.0")
-				.description("苍穹外卖项目接口文档")
-				.build();
-		Docket docket = new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(apiInfo)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.sky.controller"))
-				.paths(PathSelectors.any())
-				.build();
-		return docket;
-	}
+    /**
+     * 通过knife4j生成接口文档
+     *
+     * @return
+     */
+    @Bean
+    public Docket docket1() {
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("苍穹外卖项目接口文档")
+                .version("2.0")
+                .description("苍穹外卖项目接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("管理s端接口")
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sky.controller.admin"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
 
-	/**
-	 * 设置静态资源映射
-	 *
-	 * @param registry
-	 */
-	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
+    @Bean
+    public Docket docket2() {
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("苍穹外卖项目接口文档")
+                .version("2.0")
+                .description("苍穹外卖项目接口文档")
+                .build();
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .groupName("用户端接口")
+                .apiInfo(apiInfo)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sky.controller.user"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
 
-	/**
-	 * 扩展spring mvc框架的消息转化器
-	 *
-	 * @param converters
-	 */
-	@Override
-	protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-		log.info("扩展消息转换器....");
-		//创建一个消息转换器对象
-		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter ();
-		//需要为消息转换器设置一个对象转换器对象转换器可以将java对象序列号为json数据
-		converter.setObjectMapper(new JacksonObjectMapper());
-		//将自己的消息转换器添加到容器中
-		converters.add(0, converter);
-	}
+    /**
+     * 设置静态资源映射
+     *
+     * @param registry
+     */
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    /**
+     * 扩展spring mvc框架的消息转化器
+     *
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("扩展消息转换器....");
+        //创建一个消息转换器对象
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        //需要为消息转换器设置一个对象转换器对象转换器可以将java对象序列号为json数据
+        converter.setObjectMapper(new JacksonObjectMapper());
+        //将自己的消息转换器添加到容器中
+        converters.add(0, converter);
+    }
 }
