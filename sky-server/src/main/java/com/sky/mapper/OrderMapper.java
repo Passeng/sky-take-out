8b0a8780fc508f282cf7470926110f0b60a44efa
Passeng <1151753695@qq.com>
2023-11-13 17:23:01 +0800

    微信支付功能

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface OrderMapper {
@@ -11,4 +12,20 @@ public interface OrderMapper {
      * @param orders
      */
     void insert(Orders orders);
+
+    /**
+     * 根据订单号查询订单
+     *
+     * @param outTradeNo
+     * @return
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String outTradeNo);
+
+    /**
+     * 更新数据
+     *
+     * @param orders
+     */
+    void update(Orders orders);
 }
