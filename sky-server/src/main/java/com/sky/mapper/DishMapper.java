f9813b939045dfe0d9b95e11a6356fa6e1f8de43
Passeng <1151753695@qq.com>
2023-11-21 22:14:35 +0800

    工作台业务功能

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -10,89 +10,98 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface DishMapper {
 
-    /**
-     * 根据分类id查询菜品数量
-     *
-     * @param categoryId
-     * @return
-     */
-    @Select("select count(id) from dish where category_id = #{categoryId}")
-    Integer countByCategoryId(Long categoryId);
+	/**
+	 * 根据分类id查询菜品数量
+	 *
+	 * @param categoryId
+	 * @return
+	 */
+	@Select("select count(id) from dish where category_id = #{categoryId}")
+	Integer countByCategoryId(Long categoryId);
 
-    /**
-     * 添加菜品
-     *
-     * @param dish
-     */
-    @AutoFill(value = OperationType.INSERT)
-    void saveDish(Dish dish);
+	/**
+	 * 添加菜品
+	 *
+	 * @param dish
+	 */
+	@AutoFill(value = OperationType.INSERT)
+	void saveDish(Dish dish);
 
-    /**
-     * 分页查询菜品
-     *
-     * @param dishPageQueryDTO
-     * @return
-     */
-    Page<DishVO> queryDishPage(DishPageQueryDTO dishPageQueryDTO);
+	/**
+	 * 分页查询菜品
+	 *
+	 * @param dishPageQueryDTO
+	 * @return
+	 */
+	Page<DishVO> queryDishPage(DishPageQueryDTO dishPageQueryDTO);
 
-    /**
-     * 统计起售状态的菜品数量
-     *
-     * @param ids
-     * @return
-     */
-    Integer countEnablesByIds(List<Long> ids);
+	/**
+	 * 统计起售状态的菜品数量
+	 *
+	 * @param ids
+	 * @return
+	 */
+	Integer countEnablesByIds(List<Long> ids);
 
-    /**
-     * 批量删除菜品
-     *
-     * @param ids
-     */
-    void deleteDishBatch(List<Long> ids);
+	/**
+	 * 批量删除菜品
+	 *
+	 * @param ids
+	 */
+	void deleteDishBatch(List<Long> ids);
 
-    /**
-     * 修改菜品
-     *
-     * @param dish
-     */
-    @AutoFill(value = OperationType.UPDATE)
-    void update(Dish dish);
+	/**
+	 * 修改菜品
+	 *
+	 * @param dish
+	 */
+	@AutoFill(value = OperationType.UPDATE)
+	void update(Dish dish);
 
-    /**
-     * 根据id查询菜品
-     *
-     * @param id
-     * @return
-     */
-    @Select("select * from dish where id = #{id}")
-    DishVO queryById(Long id);
+	/**
+	 * 根据id查询菜品
+	 *
+	 * @param id
+	 * @return
+	 */
+	@Select("select * from dish where id = #{id}")
+	DishVO queryById(Long id);
 
-    /**
-     * 根据套餐id查询菜品
-     *
-     * @param setmealId
-     * @return
-     */
-    @Select("select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
-    List<Dish> getBySetmealId(Long setmealId);
+	/**
+	 * 根据套餐id查询菜品
+	 *
+	 * @param setmealId
+	 * @return
+	 */
+	@Select("select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
+	List<Dish> getBySetmealId(Long setmealId);
 
-    /**
-     * 动态条件查询菜品
-     *
-     * @param dish
-     * @return
-     */
-    List<Dish> getBycategoryId(Dish dish);
+	/**
+	 * 动态条件查询菜品
+	 *
+	 * @param dish
+	 * @return
+	 */
+	List<Dish> getBycategoryId(Dish dish);
 
-    /**
-     * 根据查询条件查询List
-     *
-     * @param dish
-     * @return
-     */
-    List<Dish> list(Dish dish);
+	/**
+	 * 根据查询条件查询List
+	 *
+	 * @param dish
+	 * @return
+	 */
+	List<Dish> list(Dish dish);
+
+	/**
+	 * 根据条件统计菜品数量
+	 *
+	 * @param map
+	 * @return
+	 */
+	Integer countByMap(Map map);
 }
--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -13,90 +13,99 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
 
-    /**
-     * 根据分类id查询套餐的数量
-     *
-     * @param id
-     * @return
-     */
-    @Select("select count(id) from setmeal where category_id = #{categoryId}")
-    Integer countByCategoryId(Long id);
+	/**
+	 * 根据分类id查询套餐的数量
+	 *
+	 * @param id
+	 * @return
+	 */
+	@Select("select count(id) from setmeal where category_id = #{categoryId}")
+	Integer countByCategoryId(Long id);
 
-    /**
-     * 统计删除菜品中有套餐关联的数量
-     *
-     * @param ids
-     * @return
-     */
-    Integer countByDishIds(List<Long> ids);
+	/**
+	 * 统计删除菜品中有套餐关联的数量
+	 *
+	 * @param ids
+	 * @return
+	 */
+	Integer countByDishIds(List<Long> ids);
 
-    /**
-     * 动态条件查询套餐
-     *
-     * @param setmeal
-     * @return
-     */
-    List<Setmeal> list(Setmeal setmeal);
+	/**
+	 * 动态条件查询套餐
+	 *
+	 * @param setmeal
+	 * @return
+	 */
+	List<Setmeal> list(Setmeal setmeal);
 
-    /**
-     * 根据套餐id查询菜品选项
-     *
-     * @param setmealId
-     * @return
-     */
-    @Select("select sd.name, sd.copies, d.image, d.description " +
-            "from setmeal_dish sd left join dish d on sd.dish_id = d.id " +
-            "where sd.setmeal_id = #{setmealId}")
-    List<DishItemVO> getDishItemBySetmealId(Long setmealId);
+	/**
+	 * 根据套餐id查询菜品选项
+	 *
+	 * @param setmealId
+	 * @return
+	 */
+	@Select("select sd.name, sd.copies, d.image, d.description " +
+			"from setmeal_dish sd left join dish d on sd.dish_id = d.id " +
+			"where sd.setmeal_id = #{setmealId}")
+	List<DishItemVO> getDishItemBySetmealId(Long setmealId);
 
-    /**
-     * 新增套餐
-     *
-     * @param setmeal
-     * @return
-     */
-    @AutoFill(OperationType.INSERT)
-    @Insert("insert into setmeal(category_id, name, price, status, description, image, create_time, update_time, create_user, update_user)" +
-            "values" +
-            "(#{categoryId}, #{name}, #{price}, #{status}, #{description}, #{image}, #{createTime}, #{updateTime},#{createUser}, #{updateUser})")
-    void insert(Setmeal setmeal);
-
-
-    /**
-     * 套餐分页查询
-     *
-     * @return
-     */
-    Page<SetmealVO> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+	/**
+	 * 新增套餐
+	 *
+	 * @param setmeal
+	 * @return
+	 */
+	@AutoFill(OperationType.INSERT)
+	@Insert("insert into setmeal(category_id, name, price, status, description, image, create_time, update_time, create_user, update_user)" +
+			"values" +
+			"(#{categoryId}, #{name}, #{price}, #{status}, #{description}, #{image}, #{createTime}, #{updateTime},#{createUser}, #{updateUser})")
+	void insert(Setmeal setmeal);
 
 
-    /**
-     * 根据id查询套餐
-     *
-     * @param id
-     * @return
-     */
-    @Select("select * from setmeal where id = #{id}")
-    Setmeal getById(Long id);
+	/**
+	 * 套餐分页查询
+	 *
+	 * @return
+	 */
+	Page<SetmealVO> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 
 
-    /**
-     * 根据id删除套餐
-     *
-     * @param setmealId
-     */
-    @Delete("delete from setmeal where id = #{id}")
-    void deleteById(Long setmealId);
+	/**
+	 * 根据id查询套餐
+	 *
+	 * @param id
+	 * @return
+	 */
+	@Select("select * from setmeal where id = #{id}")
+	Setmeal getById(Long id);
 
-    /**
-     * 修改套餐
-     *
-     * @param setmeal
-     */
-    @AutoFill(OperationType.UPDATE)
-    void update(Setmeal setmeal);
+
+	/**
+	 * 根据id删除套餐
+	 *
+	 * @param setmealId
+	 */
+	@Delete("delete from setmeal where id = #{id}")
+	void deleteById(Long setmealId);
+
+	/**
+	 * 修改套餐
+	 *
+	 * @param setmeal
+	 */
+	@AutoFill(OperationType.UPDATE)
+	void update(Setmeal setmeal);
+
+	/**
+	 * 根据条件统计套餐数量
+	 *
+	 * @param map
+	 * @return
+	 */
+	Integer countByMap(Map map);
 }
