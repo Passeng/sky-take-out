b22f14cc4184f6052374fee9bfe2493a9f977ecd
Passeng <1151753695@qq.com>
2023-11-22 16:22:43 +0800

    管理端订单相关功能

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.dto.GoodsSalesDTO;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -11,70 +13,95 @@ import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
-	/**
-	 * 插入订单数据
-	 *
-	 * @param orders
-	 */
-	void insert(Orders orders);
+    /**
+     * 插入订单数据
+     *
+     * @param orders
+     */
+    void insert(Orders orders);
 
-	/**
-	 * 根据订单号查询订单
-	 *
-	 * @param outTradeNo
-	 * @return
-	 */
-	@Select("select * from orders where number = #{orderNumber}")
-	Orders getByNumber(String outTradeNo);
+    /**
+     * 根据订单号查询订单
+     *
+     * @param outTradeNo
+     * @return
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String outTradeNo);
 
-	/**
-	 * 更新数据
-	 *
-	 * @param orders
-	 */
-	void update(Orders orders);
+    /**
+     * 更新数据
+     *
+     * @param orders
+     */
+    void update(Orders orders);
 
-	/**
-	 * 根据订单状态和下单时间查询订单
-	 *
-	 * @param status
-	 * @param orderTime
-	 * @return
-	 */
-	@Select("select * from orders where status = #{status} and order_time = #{orderTime}")
-	List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+    /**
+     * 根据订单号和用户id查询订单
+     *
+     * @param orderNumber
+     * @param userId
+     */
+    @Select("select * from orders where number = #{orderNumber} and user_id= #{userId}")
+    Orders getByNumberAndUserId(String orderNumber, Long userId);
 
-	/**
-	 * 通过id获取订单
-	 *
-	 * @param id
-	 * @return
-	 */
-	@Select("select * from orders where id = #{id};")
-	Orders getById(Long id);
+    /**
+     * 分页条件查询并按下单时间排序
+     *
+     * @param ordersPageQueryDTO
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
 
-	/**
-	 * 通过动态统计营业额数据
-	 *
-	 * @param map
-	 * @return
-	 */
-	Double sumByMap(Map map);
+    /**
+     * 根据状态统计订单数量
+     *
+     * @param status
+     */
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer status);
 
-	/**
-	 * 通过动态统计订单数据
-	 *
-	 * @param map
-	 * @return
-	 */
-	Integer countByMap(Map map);
 
-	/**
-	 * 统计指定时间区间内的销量排名前10
-	 *
-	 * @param begin
-	 * @param end
-	 * @return
-	 */
-	List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
+    /**
+     * 根据订单状态和下单时间查询订单
+     *
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from orders where status = #{status} and order_time = #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
+
+    /**
+     * 通过id获取订单
+     *
+     * @param id
+     * @return
+     */
+    @Select("select * from orders where id = #{id};")
+    Orders getById(Long id);
+
+    /**
+     * 通过动态统计营业额数据
+     *
+     * @param map
+     * @return
+     */
+    Double sumByMap(Map map);
+
+    /**
+     * 通过动态统计订单数据
+     *
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
+
+    /**
+     * 统计指定时间区间内的销量排名前10
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
